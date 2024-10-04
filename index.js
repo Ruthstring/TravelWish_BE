da8343f8839b54b0a4cbbe32060feada905ea778
Ruth Cuellar <ruthcllp@gmail.com>
2024-10-04 21:03:52 +0200

    cors for deployment

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app =express();
 require("dotenv").config();
 
+const allowedOrigins=['http://localhost:8000', 'https://travelwish-be.onrender.com'];
+
 const countryRoutes = require("./routes/countryRoutes");
 const mapRoutes = require("./routes/mapRoutes")
 const pictureRoutes=require("./routes/pictureRoutes")
@@ -11,7 +13,18 @@ const pictureRoutes=require("./routes/pictureRoutes")
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors());
+
+
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+}));
+
 
 app.get("/", (req,res)=>{
     res.send("Hello World");
